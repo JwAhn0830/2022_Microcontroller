14ffde93c892067d8474da25eb339c8d50d02fa7
JaeandWon <87607421+JaeandWon@users.noreply.github.com>
2022-12-05 02:05:19 +0900

    Timer: Overflow Interrupt, LED 0.5초마다 점멸

--- TimerwithOverflowInterrupt/TimerwithOverflowInterrupt/main.c
+++ TimerwithOverflowInterrupt/TimerwithOverflowInterrupt/main.c
@@ -4,7 +4,6 @@
  * Created: 2022-12-04-일요일 오전 4:11:17
  * Author : jaewon
  */ 
-
 #define F_CPU 16000000L
 #include <avr/io.h>
 #include <avr/interrupt.h>
@@ -15,7 +14,7 @@ volatile int count = 0;
 ISR(TIMER0_OVF_vect) {
 	count++;
 	
-	if (count >= 122) { // 0.5s
+	if (count >= 31250) { // 0.5s
 		count = 0;
 		PORTB = PORTB ^ 0x01;	
 	}
@@ -23,13 +22,12 @@ ISR(TIMER0_OVF_vect) {
 int main(void)
 {
 	DDRB = 0x01;
-    PORTB |= 0x01;
+    PORTB |= 0x00;
 	
-	TCCR0 |= (1 << CS02) | (1 << CS01); // clock soucre == 256	
+	TCCR0 |= (1 << CS00); // clock soucre == 1	
 	TIMSK |= (1 << TOIE0); // Overflow interrupt enable	
 	sei(); // global interrupt enable
     while (1) 
-    {
+	{
     }
 }
-
